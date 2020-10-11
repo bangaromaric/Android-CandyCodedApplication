3dec7733074b5f0c8879b72c4f521b2f7ae04d2d
Romaric  BANGA <bangaromaric@gmail.com>
2020-10-11 12:43:47 +0100

    add feature to lanch map

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,8 +1,10 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -14,20 +16,39 @@ public class InfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
-        Uri uri = Uri.parse("android.resource://com.codeschool.candycoded/" + R.drawable.store_front);
+        //load image
+        Uri uri = Uri.parse(getString(R.string.value_uri_image) + R.drawable.store_front);
         ImageView candyStoreImageView = (ImageView)findViewById(R.id.image_view_candy_store);
         Picasso.with(this).
                 load(uri).
                 into(candyStoreImageView);
 
-
     }
 
-    // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
-    // ***
+    /**
+     * to create and lanch map
+     * @param view android.view.View
+     */
+    public void createMapIntent(View view){
+        // Create a Uri from the address and passing in the String with the geo location of our store.
+        Uri mapUri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+
+        // Create an Intent and pass the action: Intent.ACTION_VIEW and the Uri we just created.
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, mapUri);
+
+        // Make the Intent explicit by setting the Google Maps package
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        //check that the result is not equal to null
+        if(mapIntent.resolveActivity(getPackageManager()) != null){
+            // Attempt to start an activity that can handle the Intent
+            startActivity(mapIntent);
+        }
+    }
+
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+
 }

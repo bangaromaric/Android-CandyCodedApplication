837a18d1e1e60e94350a420cee4bd3919e191ba4
Romaric  BANGA <bangaromaric@gmail.com>
2020-10-11 13:54:41 +0100

    add feature to share text

--- app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -28,13 +29,16 @@ public class DetailActivity extends AppCompatActivity {
 
         Intent intent = DetailActivity.this.getIntent();
 
-        if (intent != null && intent.hasExtra("position")) {
-            int position = intent.getIntExtra("position", 0);
+        if (intent != null && intent.hasExtra(getString(R.string.text_position))) {
+            // get position from intent
+            int position = intent.getIntExtra(getString(R.string.text_position), 0);
 
+            // fetch candy object in DB
             CandyDbHelper dbHelper = new CandyDbHelper(this);
             SQLiteDatabase db = dbHelper.getWritableDatabase();
             Cursor cursor = db.rawQuery("SELECT * FROM candy", null);
             cursor.moveToPosition(position);
+
 
             String candyName = cursor.getString(cursor.getColumnIndexOrThrow(
                     CandyContract.CandyEntry.COLUMN_NAME_NAME));
@@ -45,16 +49,19 @@ public class DetailActivity extends AppCompatActivity {
             String candyDesc = cursor.getString(cursor.getColumnIndexOrThrow(
                     CandyEntry.COLUMN_NAME_DESC));
 
-
+            // set candy name in textView
             TextView textView = (TextView) this.findViewById(R.id.text_view_name);
             textView.setText(candyName);
 
+            // set candy price in textView
             TextView textViewPrice = (TextView) this.findViewById(R.id.text_view_price);
             textViewPrice.setText(candyPrice);
 
+            // set candy description in textView
             TextView textViewDesc = (TextView) this.findViewById(R.id.text_view_desc);
             textViewDesc.setText(candyDesc);
 
+            // set candy image in ImageView
             ImageView imageView = (ImageView) this.findViewById(
                     R.id.image_view_candy);
             Picasso.with(this).load(mCandyImageUrl).into(imageView);
@@ -68,7 +75,32 @@ public class DetailActivity extends AppCompatActivity {
         return true;
     }
 
-    // ***
-    // TODO - Task 4 - Share the Current Candy with an Intent
-    // ***
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+
+        this.createShareIntent();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * to create intent and share it
+     */
+    private void createShareIntent(){
+
+        // Create an Intent with action Intent.ACTION_SEND
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+
+        // Set the type
+        shareIntent.setType("text/plain");
+
+        // Create the String that we want to share
+        String concatenated = SHARE_DESCRIPTION + mCandyImageUrl + HASHTAG_CANDYCODED;
+
+        // Use the Intent's putExtra() method to add the text we want to share.
+        shareIntent.putExtra(Intent.EXTRA_TEXT,concatenated);
+
+        // to start an activity that can handle the Intent
+        startActivity(shareIntent);
+    }
+
 }
